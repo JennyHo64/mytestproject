4983bd45cc6c11abe37efe9e0cafad2ff4cd31bf
JennyHo64 <je64yyy@gmail.com>
2018-09-22 19:23:47 +0800

    180922_m

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,77 @@
-$(document).on('click','#commit',function{
-    
-    let name = ('#name').val();
-    let age = ('#age').val();
-    let sex = ('#sex').val();
 
-    let appurl='http://localhost:3000/api';
+
+//1.新增會員
+$(document).on('click','#submit',function(){  //為何這裡要有一個function當參數
+    //alert('clicked');
+    let name = $('#name').val();
+    let age = $('#age').val();
+    let sex = $('#sex').val();
+
+    let apiUrl='http://localhost:3000/api/creat';
     let reqInit = {
         "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
         "method" : 'POST',
         "body" : JSON.stringify({
             "name_key" :name,
             "age_key" : age,
-            "sex_key" : sex
+            "sex_key" : sex,
         })  
     }
     console.log(`name: ${name} age: ${age} sex: ${sex}`);
 
-    return window.fetch(appurl,reqInit).then(function(res){
-        console.log(res);  //這裡是後端把結果丟給前端(ex:顯示出剛剛前面輸入的結果)
+    //這裡是後端把結果丟給前端(ex:顯示出剛剛前面輸入的結果)
+    return window.fetch(apiUrl,reqInit).then(function(res){
+        res.json().then(function(resJson){
+            let idInFnc=resJson.id;
+            let nameInFnc=resJson.name;
+            let ageInFnc=resJson.age;
+            let sexInFnc=resJson.sex;
+
+            let trElement=`<tr data-memberId="${idInFnc}">
+                                <th id="th-name">${nameInFnc}</th>
+                                <th id="th-age">${ageInFnc}</th>
+                                <th id="th-sex">${sexInFnc}</th>
+                            </tr>`;
+            console.log(resJson);
+            $('#member-list').append(trElement);
+
+        })
+        console.log(res);
     })
 });
+
+//2.取得會員資料
+$(document).ready(function(){
+    getMembers();
+});
+
+function getMembers(){
+    let apiUrl='http://localhost:3000/api/take'; 
+    let reqInit = {
+        "headers": new Headers({'Content-Type': 'application/json'}),//header的H要大寫!!!!!!
+        "method" : 'GET'
+    }
+
+    return window.fetch(apiUrl,reqInit).then(function(res){
+        res.json().then(function(resJson){
+            let docs =  resJson.docs;
+            docs.map(function(doc){
+                let id = doc.id;
+                let name = doc.name;
+                let age = doc.age;
+                let sex = doc.sex;
+                let trElement =  `<tr data-memberId="${id}">
+                                        <th id="th-name">${name}</th>
+                                        <th id="th-age">${age}</th>
+                                        <th id="th-sex">${sex}</th>
+                                    <td>
+                                        <button class="btn btn-primary" type="button" id="delete">刪除</button>
+                                        <button class="btn btn-primary" type="button" id="modify" data-toggle="modal" data-target="#update-modal">修改</button>
+                                    </td>
+                                </tr>`;
+                $('#member-list').append(trElement);
+            })
+            console.log(resJson);//resJson是處理完的結果
+        })
+    })
+} 
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,35 @@
 var express = require('express');
 var router = express.Router();
-aaaacccc
+var Cloudant = require('@cloudant/cloudant');
+var username = '290882c8-24eb-4919-8987-f6f8b8fdfab8-bluemix';
+var password='bd7d946c9b83b6663b65ca6ab829345c272961c5beec2869d37dd6803469bf3f';
 
-module.exports = router; //後端城市寫完都需要加這一行供其他城市使用;;;
+//1.新增會員
+router.post('/creat',function(req,res){
+    let name=req.body.name_key;
+    let age=req.body.age_key;
+    let sex=req.body.sex_key;
+    let pushData={
+        "name":name,
+        "age":age,
+        "sex":sex,
+        "timeStemp":Date.now()
+    }
+    Cloudant({account:username, password:password}, function(err, cloudant) {
+        if (err) {
+          return console.log('Failed to initialize Cloudant: ' + err.message);
+        }
+      
+        var db = cloudant.db.use("mytestdb");
+        db.insert(pushData, function(err, data) {
+            if (err) {
+                return console.log('Failed to initialize Cloudant: ' + err.message);
+              }
+            let id=data.id;
+            pushData.id=id;
+            res.status(200).json(pushData);
+        });
+      });
+});
+
+module.exports = router; //後端城市寫完都需要加這一行供其他城市使用
